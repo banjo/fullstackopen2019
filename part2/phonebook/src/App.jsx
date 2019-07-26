d97a790413a53f753eb77b2de5fea2e4f529be8a
Anton Ödman <anton.odman@gmail.com>
2019-07-26 14:36:46 +0200

    2.11 the phonebook step6

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,20 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import Numbers from "./components/Numbers";
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 
 const App = () => {
-    const [persons, setPersons] = useState([
-        { name: "Arto Hellas", number: "040-123456" },
-        { name: "Ada Lovelace", number: "39-44-5323523" },
-        { name: "Dan Abramov", number: "12-43-234345" },
-        { name: "Mary Poppendieck", number: "39-23-6423122" }
-    ]);
+    const [persons, setPersons] = useState([]);
     const [newName, setNewName] = useState("");
     const [newNumber, setNewNumber] = useState("");
     const [newFilter, setNewFilter] = useState("");
     const [filteredPersons, setFilteredPersons] = useState([]);
 
+    // effect hook for json server
+    useEffect(() => {
+        axios.get("http://localhost:3001/persons").then(response => {
+            setPersons(response.data);
+        });
+    }, []);
+
+    // add person on click
     const addPerson = event => {
         event.preventDefault();
         const allNames = persons.map(person => person.name);
