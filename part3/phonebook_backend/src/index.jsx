d8f9c88c7f44c8d766555cd1b1ef45a915237d09
Anton Ödman <anton.odman@gmail.com>
2019-08-05 17:05:14 +0200

    3.5 done

--- part3/phonebook_backend/src/index.jsx
+++ part3/phonebook_backend/src/index.jsx
@@ -2,11 +2,32 @@ const express = require("express");
 const app = express();
 const PORT = 3001;
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+
 phonebook = [
     { name: "Antonio", number: "0500050", id: 1 },
     { name: "Dubio", number: "3322", id: 2 },
     { name: "Dracunios", number: "44422", id: 3 }
 ];
+
+// function to generate a new ID
+const generateId = () => {
+    const currentIds = phonebook.map(contact => contact.id);
+    let newId;
+
+    while (true) {
+        newId = Math.floor(Math.random() * 100) + 1;
+
+        if (currentIds.includes(newId)) {
+            continue;
+        }
+
+        break;
+    }
+
+    return newId;
+};
 
 // API FOR INFO
 app.get("/info", (req, res) => {
@@ -32,6 +53,27 @@ app.get("/api/persons/:id", (req, res) => {
     } else {
         res.status(404).end();
     }
+});
+
+// ADD ENTRY
+app.post("/api/persons", (req, res) => {
+    const newContact = req.body;
+
+    if (!newContact) {
+        return res.status(400).json({
+            error: "content is missing"
+        });
+    }
+
+    const contact = {
+        name: newContact.name,
+        number: newContact.number,
+        id: generateId()
+    };
+
+    phonebook = phonebook.concat(contact);
+
+    res.json(phonebook);
 });
 
 // DELETE ENTRY
