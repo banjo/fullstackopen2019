714e797632ed8bfa474cdb32198dae52b9183924
Anton Ödman <anton.odman@gmail.com>
2019-08-07 16:19:56 +0200

    Added build and combined back- and frontend

---
+++ part3/phonebook_backend/build/precache-manifest.38af2e24b22aff8ec43a30d992177fb4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "994ee43195d873bdaa09b889a16539f3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "edb70b9760340e495c07",
+    "url": "/static/js/2.42c16a5d.chunk.js"
+  },
+  {
+    "revision": "d5b3311266a31514dd83",
+    "url": "/static/js/main.11650755.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
