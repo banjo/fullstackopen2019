5d22f2b76af298ff2f5c591289982571bb1bc73c
Anton Ödman <anton.odman@gmail.com>
2019-09-05 11:16:44 +0200

    Initiated supertest and refactored logger

--- part4/blog_list/app.js
+++ part4/blog_list/app.js
@@ -1,28 +1,27 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const mongoose = require("mongoose");
-const morgan = require("morgan");
-const config = require("./utils/config");
-const blogsRouter = require("./controllers/blogs");
-const middleware = require("./utils/middleware");
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const config = require('./utils/config');
+const blogsRouter = require('./controllers/blogs');
+const middleware = require('./utils/middleware');
+const logger = require('./utils/logger');
 
-console.log("Connecting to", config.MONGODB_URL);
+logger.info('Connecting to', config.MONGODB_URL);
 
 mongoose
-    .connect(config.MONGODB_URL, { useNewUrlParser: true })
-    .then(() => console.log("Conntected to MongoDB"))
-    .catch(error => {
-        console.log("Error connecting to MongoDB:", error.message);
-    });
+	.connect(config.MONGODB_URL, { useNewUrlParser: true })
+	.then(() => logger.info('Conntected to MongoDB'))
+	.catch((error) => logger.error('Error connecting to MongoDB:', error.message));
 
 app.use(middleware.requestLogger);
-app.use(morgan("tiny"));
+app.use(morgan('tiny'));
 app.use(cors());
 app.use(bodyParser.json());
 
-app.use("/api", blogsRouter);
+app.use('/api', blogsRouter);
 
 app.use(middleware.unknownEndpoint);
 
---
+++ part4/blog_list/tests/blog_api.test.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const supertest = require('supertest');
+const app = require('../app');
+
+const api = supertest(app);
+
+test('blog listings are returned as json', async () => {
+	await api.get('/api/blogs').expect(200).expect('Content-Type', /application\/json/);
+});
+
+afterAll(() => {
+	mongoose.connection.close();
+});
