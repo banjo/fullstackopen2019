12573c5884d6f1100feca22ac1090aa114cb9678
Anton <anton.odman@gmail.com>
2019-09-09 22:08:24 +0200

    Re-added assignment

--- part5/bloglist_frontend/src/App.js
+++ part5/bloglist_frontend/src/App.js
@@ -1,8 +1,12 @@
-import React from 'react';
-// import logo from './logo.svg';
-// import './App.css';
+import React, { useEffect, useState } from 'react';
+import getAll from './services/blogs';
+import axios from 'axios';
 
 function App() {
+	useEffect(() => {
+		axios.get('/api/blogs').then((res) => console.log(res.data));
+	}, []);
+
 	return <div>hello</div>;
 }
 
