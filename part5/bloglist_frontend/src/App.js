8ba6176aa8f991dac781f1a2776ba8c63cf3643d
Anton Ödman <anton.odman@gmail.com>
2019-09-10 15:01:19 +0200

    5.2 done

--- part5/bloglist_frontend/src/App.js
+++ part5/bloglist_frontend/src/App.js
@@ -1,13 +1,84 @@
 import React, { useEffect, useState } from 'react';
-import getAll from './services/blogs';
-import axios from 'axios';
+import blogService from './services/blogs';
+// import axios from 'axios';
+import Blog from './components/Blog';
+import LoginForm from './components/LoginForm';
+import loginService from './services/loginService';
 
 function App() {
+	const [ blogs, setBlogs ] = useState([ {} ]);
+	const [ user, setUser ] = useState(null);
+	const [ login, setLogin ] = useState({ username: '', password: '' });
+
+	// get all blogs
 	useEffect(() => {
-		axios.get('/api/blogs').then((res) => console.log(res.data));
+		const getBlogs = async () => {
+			const allBlogs = await blogService.getAll();
+			setBlogs(allBlogs);
+		};
+		getBlogs();
 	}, []);
 
-	return <div>hello</div>;
+	// get user if stored locally
+	useEffect(() => {
+		const loggedUserJSON = window.localStorage.getItem('loggedUser');
+		if (loggedUserJSON) {
+			const user = JSON.parse(loggedUserJSON);
+			setUser(user);
+		}
+	}, []);
+
+	// handlers
+	const loginHandler = async (event) => {
+		event.preventDefault();
+
+		try {
+			const user = await loginService.login(login.username, login.password);
+
+			// save user locally
+			window.localStorage.setItem('loggedUser', JSON.stringify(user));
+
+			setUser(user);
+			setLogin({ username: '', password: '' });
+		} catch (error) {
+			//TODO: add response
+			console.log('Wrong credentials');
+		}
+    };
+
+    const logoutHandler = async () => {
+        window.localStorage.clear()
+        setUser(null)
+    }
+
+	// turn blogs to HTML
+	const blogItems = blogs.map((blog, index) => <Blog key={index} blog={blog} />);
+
+	// return login if not logged in
+	if (user === null) {
+		return (
+			<div>
+				<h2>Log in</h2>
+				<LoginForm loginHandler={loginHandler} setLogin={setLogin} login={login} />
+			</div>
+		);
+	}
+
+	// return blogs if logged in
+	return (
+		<div className="App">
+			<div>
+				<h3>Blogs</h3>
+				<div>
+					{user.name} logged in
+					<input type="button" value="logout" onClick={logoutHandler} />
+				</div>
+				<br />
+
+				<div>{blogItems}</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
---
+++ part5/bloglist_frontend/src/components/LoginForm.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const LoginForm = ({ loginHandler, setLogin, login }) => {
+	const handleChange = (event) => {
+		// update password and username
+		if (event.target.name === 'password') {
+			setLogin({ ...login, password: event.target.value });
+		} else if (event.target.name === 'username') {
+			setLogin({ ...login, username: event.target.value });
+		}
+	};
+
+	return (
+		<form onSubmit={loginHandler}>
+			<div>
+				Username <input type="text" name="username" onChange={handleChange} />
+			</div>
+			<div>
+				Password <input type="password" name="password" onChange={handleChange} />
+			</div>
+			<div>
+				<input type="submit" value="Login" />
+			</div>
+		</form>
+	);
+};
+
+export default LoginForm;
