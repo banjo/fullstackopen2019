261cd9266a4ca4a29aa010480b5c9399dccbd25b
Anton Ödman <anton.odman@gmail.com>
2019-09-12 13:39:09 +0200

    5.6 done

--- part5/bloglist_frontend/src/components/Blog.js
+++ part5/bloglist_frontend/src/components/Blog.js
@@ -1,8 +1,38 @@
-import React from 'react';
-const Blog = ({ blog }) => (
-	<div>
-		{blog.title} {blog.author}
-	</div>
-);
+import React, { useState } from 'react';
+const Blog = ({ blog }) => {
+	const [ expand, setExpand ] = useState(false);
+
+	const blogStyle = {
+		paddingTop   : 10,
+		paddingLeft  : 2,
+		border       : 'solid',
+		borderRadius : 5,
+		borderWidth  : 1,
+		marginBottom : 5
+	};
+
+	if (expand) {
+		return (
+			<div style={blogStyle}>
+				<div onClick={() => setExpand(false)}>
+					<div>{blog.title}</div>
+					<div>{blog.url}</div>
+					<div>
+						{blog.likes} likes <button>Like</button>
+					</div>
+					<div>Added by {blog.author}</div>
+				</div>
+			</div>
+		);
+	}
+
+	return (
+		<div style={blogStyle}>
+			<div onClick={() => setExpand(true)}>
+				{blog.title} {blog.author}
+			</div>
+		</div>
+	);
+};
 
 export default Blog;
