9b3eec9636a9e00dd4fc89b4299ec52e28e43a59
Anton Ödman <anton.odman@gmail.com>
2019-09-13 15:06:57 +0200

    5.14 done

--- part5/bloglist_frontend/src/components/SimpleBlog.test.js
+++ part5/bloglist_frontend/src/components/SimpleBlog.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
-import { render, cleanup } from '@testing-library/react';
+import { render, cleanup, fireEvent } from '@testing-library/react';
 import SimpleBlog from './SimpleBlog';
 
 afterEach(cleanup);
@@ -17,4 +17,22 @@ test('renders all content', () => {
     expect(component.container).toHaveTextContent('google');
     expect(component.container).toHaveTextContent('jafar');
     expect(component.container).toHaveTextContent('2');
+});
+
+test('Click button twice', () => {
+    const blog = {
+        title  : 'google',
+        author : 'jafar',
+        likes  : 2
+    };
+
+    const mockHandler = jest.fn();
+
+    const { getByText } = render(<SimpleBlog blog={blog} onClick={mockHandler} />);
+
+    const button = getByText('like');
+    fireEvent.click(button);
+    fireEvent.click(button);
+
+    expect(mockHandler.mock.calls.length).toBe(2);
 });
