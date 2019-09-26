ec1aaff5a095fa03efc2fd5b37a3adf1c78f76e9
Anton Ödman <anton.odman@gmail.com>
2019-09-26 15:28:33 +0200

    6.4 done

---
+++ part6/redux-anecdotes/src/App.js
@@ -0,0 +1,47 @@
+import React from 'react';
+
+const App = (props) => {
+    const anecdotes = props.store.getState();
+
+    const vote = (id) => {
+        console.log('vote', id);
+        props.store.dispatch({
+            type : 'INCREMENT',
+            data : { id }
+        });
+    };
+
+    const addAnecdote = (event) => {
+        event.preventDefault();
+        const anecdote = event.target.anecdote.value;
+        props.store.dispatch({
+            type : 'ADD',
+            data : { anecdote }
+        });
+        console.log(anecdote);
+    };
+
+    return (
+        <div>
+            <h2>Anecdotes</h2>
+            {anecdotes.map((anecdote) => (
+                <div key={anecdote.id}>
+                    <div>{anecdote.content}</div>
+                    <div>
+                        has {anecdote.votes}
+                        <button onClick={() => vote(anecdote.id)}>vote</button>
+                    </div>
+                </div>
+            ))}
+            <h2>create new</h2>
+            <form onSubmit={addAnecdote}>
+                <div>
+                    <input name="anecdote" />
+                </div>
+                <button>create</button>
+            </form>
+        </div>
+    );
+};
+
+export default App;
