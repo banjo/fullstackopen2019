b0df7b815ce204bb38d850d814fe07c17813bee1
Anton Ödman <anton.odman@gmail.com>
2019-10-09 12:26:21 +0200

    7.7 done

--- part7/bloglist/client/src/components/Users.js
+++ part7/bloglist/client/src/components/Users.js
@@ -2,15 +2,58 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 const Users = (props) => {
-    const userInfo = props.blogs.map((blog) => {
-        return <div>{blog.title}</div>;
+    const getUsers = (blogs) => {
+        // get unique users
+        const uniqueIds = [ ...new Set(blogs.map((blog) => blog.user.id)) ];
+
+        // loop through ids to get all stats
+        const userStats = uniqueIds.map((id) => {
+            let currentUser = {
+                id            : id,
+                amountOfBlogs : 0
+            };
+
+            // loop through blogs and compare to ID
+            blogs.forEach((blog) => {
+                if (blog.user.id === id) {
+                    currentUser = {
+                        ...currentUser,
+                        name          : blog.user.name,
+                        amountOfBlogs : currentUser.amountOfBlogs + 1
+                    };
+                }
+            });
+
+            return currentUser;
+        });
+
+        return userStats;
+    };
+
+    const userData = getUsers(props.blogs);
+
+    const userInfo = userData.map((user, index) => {
+        return (
+            <tr key={index}>
+                <th>{user.name}</th>
+                <th>{user.amountOfBlogs}</th>
+            </tr>
+        );
     });
 
     return (
         <div>
-            Hello from Users
+            <h2>Users</h2>
             <br />
-            {userInfo}
+            <table>
+                <thead>
+                    <tr>
+                        <th>User</th>
+                        <th>Blogs created</th>
+                    </tr>
+                </thead>
+                <tbody>{userInfo}</tbody>
+            </table>
         </div>
     );
 };
