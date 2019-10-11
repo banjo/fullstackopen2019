5aa6a30edce4c2fd5c84edee56b89d7d36665f49
Anton Ödman <anton.odman@gmail.com>
2019-10-11 11:21:02 +0200

    7.16 done

--- part7/bloglist/client/webpack.config.js
+++ part7/bloglist/client/webpack.config.js
@@ -1,10 +1,36 @@
 const path = require('path');
 
 const config = {
-    entry  : [ '@babel/polyfill', './src/index.js' ],
-    output : {
+    entry     : [ '@babel/polyfill', './src/index.js' ],
+    output    : {
         path     : path.resolve(__dirname, 'build'),
         filename : 'main.js'
+    },
+    devServer : {
+        contentBase : path.resolve(__dirname, 'build'),
+        compress    : true,
+        port        : 3001,
+        proxy       : { '/api': 'http://localhost:3003' }
+    },
+    devtool   : 'source-map',
+    module    : {
+        rules : [
+            {
+                test   : /\.js$/,
+                loader : 'babel-loader',
+                query  : {
+                    presets : [ '@babel/preset-env', '@babel/preset-react' ]
+                }
+            },
+            {
+                test    : /\.css$/,
+                loaders : [ 'style-loader', 'css-loader' ]
+            },
+            {
+                test : /\.scss$/,
+                use  : [ 'style-loader', 'css-loader', 'sass-loader' ]
+            }
+        ]
     }
 };
 
